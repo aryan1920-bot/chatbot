c834d4c92ebc63a75ae0be13fd4e589a0d3a73ca
Aryan <singlaaryan1902@gmail.com>
2023-08-28 13:16:37 +0530

    updated title

--- papp.py
+++ papp.py
@@ -65,8 +65,8 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="personal Chatbot",
+                       page_icon=":sparkles:")
     st.write(css, unsafe_allow_html=True)
 
 
